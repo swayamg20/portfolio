11365a258c590313b6d0b5f61210878dc30e1713
swayam.gupta <swayam.gupta@travenues.com>
2025-09-07 02:17:22 +0530

    deployement planning

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  output: 'export',
+  basePath: '/portolio',
+
   images: {
-    domains: ['cdn.magicui.design'],
+    unoptimized: true,
   },
   // Enable static exports if needed
   // output: 'export',
